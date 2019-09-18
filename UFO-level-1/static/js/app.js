f217671b37f12e88ca9cb64e382dbc1413b05b43
David Liska <liskadavid@gmail.com>
2019-09-18 16:37:13 -0500

    Corrected syntax from source examples.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,19 +12,30 @@ button.on("click", function() {
     var inputValue = inputElement.property("value");
 
 
-    var filteredData = tableData.filter(sighting => tableData.datetime === inputValue);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    var sighting_date = filteredData.map(sighting => sighting.datetime)
+    var sighting_city = filteredData.map(sighting => sighting.city)
+    var sighting_state = filteredData.map(sighting => sighting.state)
+    var sighting_country = filteredData.map(sighting => sighting.country)
+    var sighting_shape = filteredData.map(sighting => sighting.shape)
+    var sighting_comment = filteredData.map(sighting => sighting.comment)
+
+
 
     var result_table = d3.select(".table-striped");
 
-    // remove any children from the list to
+
+    // remove any children from the table
     result_table.html("");
   
-    // append stats to the list
-    list.append("td").text(`Mean: ${mean}`);
-    list.append("td").text(`Median: ${median}`);
-    list.append("td").text(`Mode: ${mode}`);
-    list.append("td").text(`Variance: ${variance}`);
-    list.append("td").text(`Standard Deviation: ${standardDeviation}`);
+    // append data to the table
+    result_table.append("td").text(`${sighting_date}`);
+    result_table.append("td").text(`${sighting_city}`);
+    result_table.append("td").text(`${sighting_state}`);
+    result_table.append("td").text(`${sighting_country}`);
+    result_table.append("td").text(`${sighting_shape}`);
+    result_table.append("td").text(`${sighting_comment}`);
 
 
 });
